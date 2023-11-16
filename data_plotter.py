b6eba6ecf08f3940195271810cc8f49048f8e05c
Prosper Ndebele <pndebele@wesleyan.edu>
2023-11-15 21:29:55 -0500

    created the skeleton code for functions that plot the data in a matplotlib

---
+++ data_plotter.py
@@ -0,0 +1,52 @@
+import matplotlib.pyplot as plt
+import datetime
+
+
+def plot_time_series(data, time_intervals, title="Data Evolution Over Time", xlabel="Time", ylabel="Data Value"):
+    """
+    Plot a time series graph.
+
+    Parameters:
+        data (list): List of data values.
+        time_intervals (list): List of time intervals corresponding to each data point.
+        title (str, optional): Title of the plot. Default is "Data Evolution Over Time".
+        xlabel (str, optional): Label for the x-axis. Default is "Time".
+        ylabel (str, optional): Label for the y-axis. Default is "Data Value".
+
+    Returns:
+        None
+    """
+
+
+def plot_scatter(data_x, data_y, title="Scatter Plot", xlabel="X-axis", ylabel="Y-axis"):
+    """
+    Plot a scatter plot.
+
+    Parameters:
+        data_x (list): List of values for the x-axis.
+        data_y (list): List of values for the y-axis.
+        title (str, optional): Title of the plot. Default is "Scatter Plot".
+        xlabel (str, optional): Label for the x-axis. Default is "X-axis".
+        ylabel (str, optional): Label for the y-axis. Default is "Y-axis".
+
+    Returns:
+        None
+    """
+
+
+def plot_bar_chart(categories, values, title="Bar Chart", xlabel="Categories", ylabel="Values"):
+    """
+    Plot a bar chart.
+
+    Parameters:
+        categories (list): List of categories.
+        values (list): List of values corresponding to each category.
+        title (str, optional): Title of the plot. Default is "Bar Chart".
+        xlabel (str, optional): Label for the x-axis. Default is "Categories".
+        ylabel (str, optional): Label for the y-axis. Default is "Values".
+
+    Returns:
+        None
+    """
+
+# Add more plotting functions as needed
